d98736831004c56afa3ba0e4fe6f3e71a226913f
Afshin <afshin.m4460@gmail.com>
2023-03-31 17:59:11 +0430

    react lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Toolbar from './containers/header/toolbar/toolbar';
 import HomePage from './pages/HomePage';
-import AddStudent from './pages/AddStudent';
 import EditStudent from './pages/EditStudent';
+const AddStudent = React.lazy(() => import('./pages/AddStudent'));
 
 function App() {
     return (
@@ -14,7 +14,11 @@ function App() {
                 <div style={{marginTop: '65px'}}>
                     <Routes>
                         <Route path='/' element={<HomePage />} />
-                        <Route path='add-student' element={<AddStudent />} />
+                        <Route path='add-student' element={
+                            <Suspense fallback={<p>...loading</p>}>
+                                <AddStudent />
+                            </Suspense>
+                        } />
                         <Route path='student/:studentId' element={<EditStudent />} />
                         <Route path='*' element={<h1>Not Found</h1>} />
                     </Routes>
